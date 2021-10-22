bbd4adf94a275fc287ce4fdca84bd61357f5ad59
Alexander Nyman <alexandernyman@MacBook-Pro-som-tillhor-Alexander.local>
2021-10-22 21:46:51 +0200

    Created the folder structure for this project

---
+++ src/scripts/app.js
@@ -0,0 +1,4 @@
+// app.js is for all of the javascript
+// /vendors is for all 3rd party plugins, for example slick, modernizr....
+// prepend into app.js
+// all of this is sent to the build app-min.js file in build at the end.
